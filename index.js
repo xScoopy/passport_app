601565e239a504be6ae171a6944986db994cdc3f
xScoopy <JLeary070@gmail.com>
2021-05-12 20:26:21 -0500

    added routes and ensure user middleware

--- index.js
+++ index.js
@@ -37,5 +37,57 @@ const UserDetail = new Schema({
 UserDetail.plugin(passportLocalMongoose)
 const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
 
+//Passport Local Authentication
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+/* ROUTES */
+
+const connectEnsureLogin = require('connect-ensure-login');
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local',
+  (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.redirect('/login?info=' + info);
+    }
+
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.redirect('/');
+    });
+
+  })(req, res, next);
+});
+
+app.get('/login',
+  (req, res) => res.sendFile('html/login.html',
+  { root: __dirname })
+);
+
+app.get('/',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/index.html', {root: __dirname})
+);
+
+app.get('/private',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.sendFile('html/private.html', {root: __dirname})
+);
+
+app.get('/user',
+  connectEnsureLogin.ensureLoggedIn(),
+  (req, res) => res.send({user: req.user})
+);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('App listening on port ' + port));
